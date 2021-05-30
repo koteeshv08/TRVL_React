592adcd1919521dbf2b8875fc50427ec830a23ee
koteeshv08 <koteeshv08@gmail.com>
2021-05-30 13:34:41 +0530

    Initial commit

---
+++ src/components/MainSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './MainSection.css';
+import video from '../videos/video-1.mp4'
+
+export default function MainSection() {
+    return (
+        <div className='hero-container'>
+            <video src={video} autoPlay={true} loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for!!!</p>
+            <div className="hero-btns">
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WATCH TRAILER <i className='far fa-play-circle' /></Button>
+            </div>
+        </div>
+    )
+}
